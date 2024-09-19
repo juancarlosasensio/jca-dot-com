9f548f8afc1a777060c2c5273f225767563d38d3
Juan Carlos Asensio <jasensio@ufm.edu>
2024-09-19 16:26:26 -0400

    Add now page

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   eleventyConfig.setUseGitIgnore(false);
+
+  eleventyConfig.addFilter("simpleDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
+  });
 
   // For now, we want all our styles to be copied over
   eleventyConfig.addPassthroughCopy('./src/css/**/*.css');
--- src/index.md
+++
@@ -1,6 +0,0 @@
----
-title: Juan Carlos Asensio
-layout: "layouts/base.njk"
----
-
-This is my little nook on the world wide web. I've been designing websites and solving problems with code for the last 5 years. Before that, I worked on the business-side of startups and VC. I'm currently obsessed with design systems and responsiveness, the indie web, and containerization.
