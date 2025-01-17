173485571b7994847cc73d7033ac2d5069654ae1
Juan Carlos Asensio <jasensio@ufm.edu>
2025-01-17 10:13:57 -0500

    Get WP published posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
 const { DateTime } = require("luxon");
 
+ // Collections
+  const drafts = require('./src/collections/drafts.js');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("simpleDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
   });
+
+  // Returns a collection of drafts from WordPress in reverse date order
+  eleventyConfig.addCollection('drafts', drafts);
 
   // For now, we want all our styles to be copied over
   eleventyConfig.addPassthroughCopy('./src/css/**/*.css');
---
+++ src/collections/drafts.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+
+const axios = require('axios');
+const {AssetCache} = require('@11ty/eleventy-fetch');
+const site = require('../_data/site.json');
+
+module.exports = async function () {
+
+  // jcadotcomdata.wordpress.com
+
+  const url = `https://public-api.wordpress.com/rest/v1.1/sites/jcadotcomdata.wordpress.com/posts/`;
+
+  const asset = new AssetCache('drafts');
+
+  // If saved in cache, return that instead of fetching all the data
+  if (asset.isCacheValid('8h')) {
+    return asset.getCachedValue();
+  }
+
+  // Grab the first page of posts
+  let res = await axios.get(url);
+
+  // console.log(res)
+
+  // Use the header to determine how many pages there are left to get
+  const totalPages = parseInt(res.headers['x-wp-totalpages']);
+
+  // Turn the initial page into consumable JSON
+  let data = await res.data;
+
+  // Set a counter and a return array, setting the initial page of data as it's value
+  let pageCount = 0;
+  let items = data.posts;
+
+  console.log(items);
+
+  // // Loop until page limit exceeded
+  // while (pageCount < totalPages) {
+  //   pageCount++;
+
+  //   // Ditch this iteration if we're on page one
+  //   if (pageCount === 1) {
+  //     continue;
+  //   }
+
+  //   // Smoosh the rest of the data in
+  //   res = await axios.get(url + `&page=${pageCount}`);
+  //   data = await res.data;
+  //   items = [...items, data].flat();
+
+  //   continue;
+  // }
+
+  // items.forEach(item => {
+  //   // Replace images from WordPress with imgix
+  //   item.content.rendered = item.content.rendered.replace(
+  //     new RegExp(process.env.MEDIA_URL_CMS, 'g'),
+  //     process.env.MEDIA_URL_CDN
+  //   );
+  //   item.content.rendered = item.content.rendered.replace(
+  //     new RegExp(process.env.MEDIA_URL_CMS_LEGACY, 'g'),
+  //     process.env.MEDIA_URL_CDN
+  //   );
+
+  //   // Replace API url with live site url
+  //   item.content.rendered = item.content.rendered.replace(
+  //     new RegExp(process.env.WP_API, 'g'),
+  //     site.url
+  //   );
+
+  //   item.jetpack_featured_media_url = item.jetpack_featured_media_url.replace(
+  //     new RegExp(process.env.MEDIA_URL_CMS, 'g'),
+  //     process.env.MEDIA_URL_CDN
+  //   );
+  // });
+
+  // // Stick in cache for later
+  // asset.save(items, 'json');
+  return items;
+};
