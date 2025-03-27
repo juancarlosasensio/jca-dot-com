569c049ab57e737fae49c2dd7ebceae96e4d7610
Juan Carlos Asensio <jasensio@ufm.edu>
2025-03-27 10:31:40 -0400

    Add heading font

--- .eleventy.js
+++ .eleventy.js
@@ -4,33 +4,33 @@ require('dotenv').config()
 const wpContent = require('./src/collections/blog.js');
 const books = require('./src/collections/books.js');
 
-module.exports = function (eleventyConfig) {
+module.exports = function (config) {
   // Creates a global variable for the current __dirname to make including and
   // working with files in the pattern library a little easier
   global.__basedir = __dirname;
 
-  eleventyConfig.addFilter("simpleDate", (dateObj) => {
+  config.addFilter("simpleDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
   });
 
   // Get 'notes' from Wordpress content
-  eleventyConfig.addFilter("getNotes", (arr) => {
+  config.addFilter("getNotes", (arr) => {
     return arr.filter(note => note.categories.hasOwnProperty('Notes'));
   });
 
   // Get 'blog posts' from Wordpress content
-  eleventyConfig.addFilter("getPosts", (arr) => {
+  config.addFilter("getPosts", (arr) => {
     return arr.filter(note => !note.categories.hasOwnProperty('Notes'));
   });
 
   // Add all Wordpress posts as its own collection
-  eleventyConfig.addCollection('wpContent', async function collectionCallback(collectionApi) {
+  config.addCollection('wpContent', async function collectionCallback(collectionApi) {
     const allContent = await wpContent();
     return allContent;
   });
   
   // Add books as its own collection
-  eleventyConfig.addCollection('books', books);
+  config.addCollection('books', books);
 
   // Pass through fonts and images
   config.addPassthroughCopy('./src/fonts');
