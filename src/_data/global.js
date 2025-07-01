d68bd4c50d76be60dacd2f9b8be787eb5678d40e
Juan Carlos Asensio <jasensio@ufm.edu>
2025-07-01 14:21:57 -0400

    Use assetHash only in prod

--- src/_data/global.js
+++ src/_data/global.js
@@ -4,9 +4,5 @@ module.exports = {
       return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
     };
     return `${segment()}-${segment()}-${segment()}`;
-  },
-  /**
-   * Expose whether the build is running in production mode.
-   */
-  isProduction: process.env.NODE_ENV === 'production'
+  }
 };
