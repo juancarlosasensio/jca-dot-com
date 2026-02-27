0ada8d6f3832f4ec97ec47bd409bf806cdd5d4f5
Juan Carlos Asensio <jasensio@ufm.edu>
2026-02-27 11:48:33 -0500

    Fix pagination issue

--- src/blog/.11tydata.js
+++ src/blog/.11tydata.js
@@ -1,24 +1,7 @@
-const genericPostsFilter = require('../filters/genericPosts.js');
-
 module.exports = {
   layout: "layouts/content-item.njk",
   showDisclaimer: false,
   backLink: '/blog',
   backLinkText: 'blog',
-  pageStylesheets: ["../../css/wordpress.css"],
-  pagination: {
-    data: "collections.wpContent",
-    size: 1,
-    alias: "item",
-    before: function(paginationData) {
-      return genericPostsFilter(paginationData);
-    }
-  },
-  eleventyComputed: {
-    title: data => data.item?.title?.rendered || '',
-    permalink: data => {
-      if (!data.item) return false;
-      return `/${data.item.slug}/index.html`;
-    }
-  }
+  pageStylesheets: ["../../css/wordpress.css"]
 };
--- src/notes/.11tydata.js
+++ src/notes/.11tydata.js
@@ -1,23 +1,6 @@
-const notePostsFilter = require('../filters/notesPosts.js');
-
 module.exports = {
   layout: "layouts/content-item.njk",
   showDisclaimer: true,
   backLink: '/thinking',
-  backLinkText: 'Notes',
-  pagination: {
-    data: "collections.wpContent",
-    size: 1,
-    alias: "item",
-    before: function(paginationData) {
-      return notePostsFilter(paginationData);
-    }
-  },
-  eleventyComputed: {
-    title: data => data.item?.title?.rendered || '',
-    permalink: data => {
-      if (!data.item) return false;
-      return `/${data.item.slug}/index.html`;
-    }
-  }
+  backLinkText: 'Notes'
 };
